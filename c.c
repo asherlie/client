4363dc308a81a1feb77210ce461d2007c268dbe4
asherlie <lieber.asher@gmail.com>
2023-08-14 00:43:20 -0400

    implemented insert_cb()

--- c.c
+++ c.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 struct client{
     char* name, * notes;
@@ -7,8 +9,28 @@ struct client{
 };
 
 struct client_book{
-    struct client* c_buckets;
+    struct client** c_buckets;
 };
 
-int main(int a, char** b){
+void init_cb(struct client_book* cb){
+    cb->c_buckets = calloc(sizeof(struct client*), UINT8_MAX);
+}
+
+/* TODO: alphebatize */
+void insert_cb(struct client_book* cb, char* name, char* notes){
+    struct client* c = cb->c_buckets[(uint8_t)*name], * e = malloc(sizeof(struct client));
+
+    e->name = name;
+    e->notes = notes;
+    e->next = c;
+    cb->c_buckets[(uint8_t)*name] = e;
+
+}
+
+// if no matches are found, do a O(n) search through all elements
+
+int main(){
+    struct client_book cb;
+
+    init_cb(&cb);
 }
