a0a078ed7c6c3bd45ddce5d238c115f4d7a8e9d5
asherlie <lieber.asher@gmail.com>
2023-08-14 00:51:47 -0400

    expanded c.c

--- c.c
+++ c.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 
@@ -10,10 +11,12 @@ struct client{
 
 struct client_book{
     struct client** c_buckets;
+    int n_clients;
 };
 
 void init_cb(struct client_book* cb){
     cb->c_buckets = calloc(sizeof(struct client*), UINT8_MAX);
+    cb->n_clients = 0;
 }
 
 /* TODO: alphebatize */
@@ -25,6 +28,18 @@ void insert_cb(struct client_book* cb, char* name, char* notes){
     e->next = c;
     cb->c_buckets[(uint8_t)*name] = e;
 
+    ++cb->n_clients;
+}
+
+/* prints a given bucket with an optional search term */
+void print_bucket(struct client* c, char* term, FILE* fp){
+    for(struct client* cp = c; cp; cp = cp->next){
+        if(!term || strstr(cp->name, term))
+            fprintf(fp, "%s: %s\n", cp->name, cp->notes);
+    }
+}
+
+void print_clients(struct client_book* cb, char** terms, int n_terms){
 }
 
 // if no matches are found, do a O(n) search through all elements
@@ -33,4 +48,6 @@ int main(){
     struct client_book cb;
 
     init_cb(&cb);
+    insert_cb(&cb, "asher lieber", "good man");
+    insert_cb(&cb, "another name", "pretty good man");
 }
