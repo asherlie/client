b1656e3ab140f0056166c78fb95ef28ced9846c9
asherlie <lieber.asher@gmail.com>
2023-08-15 23:21:57 -0400

    added a mess of code

--- c.c
+++ c.c
@@ -129,25 +129,244 @@ void print_client(const struct client* c, FILE* fp){
 */
 
 _Bool parse_client(struct client_book* cb, FILE* fp){
-    char c;
-    char name[80];
-    char note[2048];
-    int running_nl = 0;
-    int running_txt = 0;
-    _Bool name = 1;
+    char* prev_ln = NULL;
+    char* ln;
+    size_t sz;
+    ssize_t br, prev_br = 0, prev_prev_br = 0;
+
     int idx = 0;
+    char name[80];
+    char note[9048];
+
+    // need to:
+    //  consider first two words a name
+    //  ignore leading whitespace before a name
+    //  ignore lone lines
+    //
+    //  stop parse-ing after multiple \n\n are found
+    //      IF not a lone line AND 
+    //
+    
+    _Bool zombie;
+    _Bool name_phase = 1;
+    while((br = getline(&ln, &sz, fp)) != -1){
+        zombie = 0;
+        for(int i = 0; i < 2; ++i){
+            if(ln[br-1] == '\r' || ln[br-1] == '\n'){
+                ln[--br] = 0;
+            }
+        }
+        if(name_phase && idx == 0){
+            /*
+             * while(!isalnum(*ln)){
+             *     ++ln;
+             *     --br;
+             * }
+            */
+            for(int i = 0; i < br; ++i){
+                if(isalnum(ln[i]))
+                    break;
+                ++ln;
+                --br;
+                --i;
+            }
+            /*for(char* i = ln; */
+        }
+        /*puts(ln);*/
+
+/*
+ * don't insert until
+ * we only append/insert prev_ln when we recv a new one so we know we're not reading a zombie
+*/
+        // skip leading non-alpha
+
+        /*H*/
+        // ignore zombie line
+        if(!prev_prev_br && !br){
+            /*printf("found zombie \"%s\"\n", prev_ln);*/
+            zombie = 1;
+            goto CONT;
+            /*printf("%i\n", prev_br);*/
+        }
+        // ignore empty line
+        if(!prev_br){
+            goto CONT;
+        }
+
+        // take two first words as a name
+        if(name_phase){
+            char* sp = strchr(strchr(prev_ln, ' ')+1, ' ');
+            *sp = 0;
+            /*printf("name: \"%s\"\n", prev_ln);*/
+            int nbytes = sp-prev_ln;
+            strcpy(name, prev_ln);
+            memcpy(note+idx, sp+1, prev_br-nbytes);
+            /*printf("%i\n", idx);*/
+            idx += prev_br-nbytes;
+            printf("%i\n", idx);
+            /*printf("%i-(%i) %i\n", prev_br, sp-prev_ln, idx);*/
+            name_phase = 0;
+        }
+        else{
+            /*printf("copying %s to note+%i\n", prev_ln, idx);*/
+            /*prev_br is inaccurate*/
+            memcpy(note+idx-1, prev_ln, prev_br);
+            idx += prev_br;
+            /*puts(note);*/
+        }
+
+        /*if(prev_br && !zombie)puts(prev_ln);*/
+
+    CONT:
+        prev_ln = ln;
+        prev_prev_br = prev_br;
+        prev_br = br;
+        ln = NULL;
+
+        /*if(zombie)continue;*/
+    }
+    printf("name: \"%s\"\n", name);
+    puts(note);
+}
+
+_Bool parse_client_exp(struct client_book* cb, FILE* fp){
+    char c, prev;
+    char name[80];
+    char note[9048];
+    int running_nl = 1;
+    int running_txt = 0;
+    int n_words = 0;
+    int running_nonalph = 0;
+    int running_word = 0;
+    _Bool n_phase = 1;
+    _Bool alnum;
+    int idx = 0;
+    _Bool lone_line;
+    _Bool possible_single_line = 1;
+
+    int count = 0;
 
     while((c = fgetc(fp)) != EOF){
+        /*printf("%c\n", c);*/
+        // this is catching a lot
+        // alnum is fine as long as it's not at beginning
+        // we need a \r
+        #if 0
+        alnum = isalnum(c);
+        if(alnum)running_nonalph = 0;
+        else{
+            ++running_nonalph;
+            /*++running_nl;*/
+            /*if(n_phase && running_nonalph > 1 && !(running_word || running_txt))*/
+            if(n_phase && running_nonalph && !(running_word || running_txt))
+                continue;
+        }
+        #endif
+        /*lone_line = (!idx || running_nl) && */
+        /* discard leading nonalnum chars */
+        if(idx == 0 && n_phase && !isalpha(c)){
+            puts("discarding nonalph");
+            continue;
+        }
         switch(c){
+            case '\r':
             case '\n':
+                if(running_txt){
+                    running_nl = 0;
+                }
+                puts("FOUND NL");
+                // discard single lines of text surrounded by \n
+                if(running_txt == 1){
+                    puts("running_txt == 1, discarding");
+                    // is this adequate to discard?
+                    /*(n_phase ? name : note)[idx] = 0;*/
+                    n_phase = 1;
+                    /*printf("\"%s\"\n", n_phase ? name : note);*/
+                    idx = 0;
+                }
                 ++running_nl;
+                printf("++running_nl -> %i\n", running_nl);
                 if(running_nl > 1){
                     running_txt = 0;
+                    printf("running_nl > 1, running_txt = 0\n");
+                    // if we've had consecutive \n then we're back on name
+                    if(!n_phase){
+                        printf("finished parsing client number %i\n", ++count);
+                        puts("setting idx to 0 and switching to name phase");
+                        note[idx] = 0;
+                        /*printf("note: \"%s\"\n", note);*/
+                        idx = 0;
+                        n_phase = 1;
+                        n_words = 0;
+                    }
                 }
-            defualt:
-                running_nl = 0;
+                // lone line not working, 11/_ is being considered a name
+                printf("poss single line? %i\n", possible_single_line);
+                if(possible_single_line){
+                    lone_line = 1;
+                    (n_phase ? name : note)[idx] = 0;
+                    idx = 0;
+                    printf("found a lone line: %s\n", n_phase ? name : note);
+                    possible_single_line = 0;
+                    /*possible_single_line*/
+                    /*continue;*/
+                }
+                // need to somehow increment running_txt if nl after a line of text
+                // what about lone line?
+                if(running_nl == 1){
+                    ++running_txt;
+                }
+                break;
+            case ' ':
+                if(running_word){
+                    ++n_words;
+                }
+                else{
+                    puts("skipping whitespace");
+                    continue;
+                }
+                /* consider this a name */
+                printf("found ' ', n_words == %i\n", n_words);
+                if(n_words == 2 && n_phase){
+                    name[idx] = 0;
+                    printf("name: \"%s\"\n", name);
+                    /*
+                     * okay, name is valid - i see why
+                     * we need to lookahead and see if we're on a lone line
+                     * if so, discard recent name
+                     *
+                     * although we're essentially doing that
+                    */
+
+                    idx = 0;
+                    n_phase = 0;
+                    n_words = 0;
+                    continue;
+                }
+                /*break;*/
+            default:
+                if(running_nl){
+                    possible_single_line = 1;
+                }
+                // aha, this disqualifies single lines before the line is even over
+                // this shouldn't be set to 0 until we get our new \n
+                // if newline and running_word, then we know 
+                // if newline and running_text
+                /*else possible_single_line = 0;*/
+                ++running_word;
+                /*running_nl = 0;*/
+                // need to set both running_nl and possible_single_line to 0 when appropriate
+                // i'll only know at the end of a line
+                if(n_phase){
+                    name[idx++] = c;
+                }
+                else{
+                    note[idx++] = c;
+                }
         }
+        prev = c;
     }
+    return 0;
 }
 
 _Bool read_client(struct client_book* cb, FILE* fp){
@@ -232,7 +451,7 @@ void mtest(char* fn){
     struct client_book cb;
     FILE* fp = fopen(fn, "r");
     init_cb(&cb);
-    read_client(&cb, fp);
+    parse_client(&cb, fp);
     print_clients(&cb, NULL, 0, stdout, 0);
     fclose(fp);
 }
@@ -285,7 +504,7 @@ char** parse_args(int argc, char** argv, char** flags){
 }
 
 int main(int argc, char** argv){
-    mtest(argv[1]);
+    mtest("t.txt");
     return 0;
 
     struct client_book cb;
